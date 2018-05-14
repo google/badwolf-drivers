c81a691084df88c1097f70e217d3ff90a73a5f08
Xavier Llora <xllora@gmail.com>
2018-05-14 02:29:30 +0000

    Start the driver for Google Cloud BigTable.

---
+++ bwbigtable/keys/keys.go
@@ -0,0 +1,143 @@
+// Copyright 2015 Google Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package keys implements the mechanics that maps a triple to the collection
+// of keys to be used to store triples on Google Cloud BigTable. It also
+// provides the serialized version of the triple that will be stored on the
+// table.
+package keys
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/binary"
+	"strings"
+	"time"
+
+	"github.com/google/badwolf/triple"
+	"github.com/google/badwolf/triple/predicate"
+	"github.com/pborman/uuid"
+	"fmt"
+)
+
+const (
+	graphRowPrefix              = "graph:"
+	graphMetadataColumn         = "graph:metadata"
+	immutableTripleColumnFamily = "immutable_triple"
+	temporalTripleColumnFamily  = "temporal_triple"
+)
+
+var smallerTimestamp = time.Unix(0, 0)
+
+// Indexer contains the mapping to a given row+column combination.
+type Indexer struct {
+	// Row key in BigTable.
+	Row string
+	// Column name in BigTable.
+	Column string
+	// Timestamp to use for the cell
+	Timestamp int64
+}
+
+// String returns a human readable version of the data contained on the
+// Indexer instance.
+func (i *Indexer) String() string {
+	return "<r/" + i.Row + " c/" + i.Column + " ta/" + fmt.Sprint(i.Timestamp) + ">"
+}
+
+// compose returns a composed string base on the parts provided.
+func prependPrefix(graph string, prefix string, UUIDs ...uuid.UUID) string {
+	gb := bytes.NewBufferString(graph)
+	gb.WriteString(":")
+
+	gb.WriteString(prefix)
+	if len(UUIDs)>0 {
+		gb.WriteString(":")
+	}
+
+	var sUUIDs []string
+	for _, uuid := range UUIDs {
+		sUUIDs = append(sUUIDs, uuid.String())
+	}
+	gb.WriteString(strings.Join(sUUIDs, ":"))
+
+	return gb.String()
+}
+
+// tripleColumnFamily returns the right column family to use.
+func tripleColumnFamily(p *predicate.Predicate) string {
+	if p.Type() == predicate.Immutable {
+		return immutableTripleColumnFamily
+	}
+	return temporalTripleColumnFamily
+}
+
+// cellIndexerKeys returns an indexer entry based on the provided information.
+func cellIndexerKeys(g string, rowPrefix, columnFamily, columnPrefix string, ts int64, rowUUIDs, columnUUIDs []uuid.UUID) *Indexer {
+	return &Indexer{
+		Row:       prependPrefix(g, rowPrefix, rowUUIDs...),
+		Column:    prependPrefix(columnFamily, columnPrefix),
+		Timestamp: ts,
+	}
+}
+
+// ForTriple returns all the associate indexing keys for the given triple
+// in a given graph.
+func ForTriple(g string, t *triple.Triple) []*Indexer {
+	s, p, o := t.Subject().UUID(), t.Predicate().PartialUUID(), t.Object().UUID()
+	tcf := tripleColumnFamily(t.Predicate())
+	cta := CellTimestamp(t)
+
+	return []*Indexer{
+		cellIndexerKeys(
+			g, "OSP", tcf, "SP", cta,
+			[]uuid.UUID{o, s, p},
+			[]uuid.UUID{s, p}),
+		cellIndexerKeys(
+			g, "POS", tcf, "OS", cta,
+			[]uuid.UUID{p, o, s},
+			[]uuid.UUID{o, s}),
+		cellIndexerKeys(
+			g, "SOP", tcf, "OP", cta,
+			[]uuid.UUID{s, o, p},
+			[]uuid.UUID{o, p}),
+		cellIndexerKeys(
+			g, "SPO", tcf, "PO", cta,
+			[]uuid.UUID{s, p, o},
+			[]uuid.UUID{p, o}),
+	}
+}
+
+// ForGraph returns the key for the graph entity.
+func ForGraph(g string) *Indexer {
+	return &Indexer{
+		Row:       "graph:" + g,
+		Column:    graphMetadataColumn,
+	}
+}
+
+// GraphRowPrefix returns the column family prefix including the ':'
+func GraphRowPrefix() string {
+	return graphRowPrefix
+}
+
+// CellTimestamp returns the timestamp to use for the cell.
+func CellTimestamp(t *triple.Triple) int64 {
+	if ta, err := t.Predicate().TimeAnchor(); err == nil && ta != nil {
+		return ta.UnixNano()
+	}
+	h := sha256.Sum256([]byte(t.String()))
+	nsec, _ := binary.Varint(h[:])
+	return nsec
+}
---
+++ bwbigtable/keys/keys_test.go
@@ -0,0 +1,176 @@
+// Copyright 2015 Google Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package keys
+
+import (
+	"testing"
+
+	"fmt"
+	"github.com/google/badwolf/triple"
+	"github.com/google/badwolf/triple/literal"
+	"reflect"
+	"strings"
+)
+
+const (
+	graph = "test"
+
+	textTriples = `/u<joe> "parent_of"@[] /u<mary>
+		/u<joe> "parent_of"@[] /u<peter>
+		/u<peter> "parent_of"@[] /u<john>
+		/u<peter> "parent_of"@[] /u<eve>
+		/u<peter> "bought"@[2016-01-01T00:00:00-08:00] /c<mini>
+		/u<peter> "bought"@[2016-02-01T00:00:00-08:00] /c<model s>
+		/u<peter> "bought"@[2016-03-01T00:00:00-08:00] /c<model x>
+		/u<peter> "bought"@[2016-04-01T00:00:00-08:00] /c<model y>
+		/c<mini> "is_a"@[] /t<car>
+		/c<model s> "is_a"@[] /t<car>
+		/c<model x> "is_a"@[] /t<car>
+		/c<model y> "is_a"@[] /t<car>
+		/l<barcelona> "predicate"@[] "turned"@[2016-01-01T00:00:00-08:00]
+		/l<barcelona> "predicate"@[] "turned"@[2016-02-01T00:00:00-08:00]
+		/l<barcelona> "predicate"@[] "turned"@[2016-03-01T00:00:00-08:00]
+		/l<barcelona> "predicate"@[] "turned"@[2016-04-01T00:00:00-08:00]
+		`
+)
+
+func mustParse(t *testing.T, raw string) *triple.Triple {
+	tpl, err := triple.Parse(raw, literal.DefaultBuilder())
+	if err != nil {
+		t.Fatal(err)
+	}
+	return tpl
+}
+
+func TestForGraph(t *testing.T) {
+	table := []*struct {
+		tpl  *triple.Triple
+		want []*Indexer
+	}{
+		{
+			tpl: mustParse(t, `/u<joe> "parent_of"@[] /u<mary>`),
+			want: []*Indexer{
+				{
+					Row:       "test:OSP:e5364b94-fe66-5b01-a526-5fdf1e028e80:2821fefc-a86e-597b-b187-58805cad8813:91b6c9d2-69e3-5cec-a465-d200f4872671",
+					Column:    "immutable_triple:SP",
+					Timestamp: 36,
+				},
+				{
+					Row:       "test:POS:91b6c9d2-69e3-5cec-a465-d200f4872671:e5364b94-fe66-5b01-a526-5fdf1e028e80:2821fefc-a86e-597b-b187-58805cad8813",
+					Column:    "immutable_triple:OS",
+					Timestamp: 36,
+				},
+				{
+					Row:       "test:SOP:2821fefc-a86e-597b-b187-58805cad8813:e5364b94-fe66-5b01-a526-5fdf1e028e80:91b6c9d2-69e3-5cec-a465-d200f4872671",
+					Column:    "immutable_triple:OP",
+					Timestamp: 36,
+				},
+				{
+					Row:       "test:SPO:2821fefc-a86e-597b-b187-58805cad8813:91b6c9d2-69e3-5cec-a465-d200f4872671:e5364b94-fe66-5b01-a526-5fdf1e028e80",
+					Column:    "immutable_triple:PO",
+					Timestamp: 36,
+				},
+			},
+		},
+		{
+			tpl: mustParse(t, `/u<joe> "parent_of"@[] /u<anne>`),
+			want: []*Indexer{
+				{
+					Row:       "test:OSP:adbb5f67-23f0-56a6-82a0-4bc08546ae60:2821fefc-a86e-597b-b187-58805cad8813:91b6c9d2-69e3-5cec-a465-d200f4872671",
+					Column:    "immutable_triple:SP",
+					Timestamp: 6,
+				},
+				{
+					Row:       "test:POS:91b6c9d2-69e3-5cec-a465-d200f4872671:adbb5f67-23f0-56a6-82a0-4bc08546ae60:2821fefc-a86e-597b-b187-58805cad8813",
+					Column:    "immutable_triple:OS",
+					Timestamp: 6,
+				},
+				{
+					Row:       "test:SOP:2821fefc-a86e-597b-b187-58805cad8813:adbb5f67-23f0-56a6-82a0-4bc08546ae60:91b6c9d2-69e3-5cec-a465-d200f4872671",
+					Column:    "immutable_triple:OP",
+					Timestamp: 6,
+				},
+				{
+					Row:       "test:SPO:2821fefc-a86e-597b-b187-58805cad8813:91b6c9d2-69e3-5cec-a465-d200f4872671:adbb5f67-23f0-56a6-82a0-4bc08546ae60",
+					Column:    "immutable_triple:PO",
+					Timestamp: 6,
+				},
+			},
+		},
+		{
+			tpl: mustParse(t, `/u<joe> "parent_of"@[2017-10-01T12:00:00-08:00] /u<mary>`),
+			want: []*Indexer{
+				{
+					Row:       "test:OSP:e5364b94-fe66-5b01-a526-5fdf1e028e80:2821fefc-a86e-597b-b187-58805cad8813:91b6c9d2-69e3-5cec-a465-d200f4872671",
+					Column:    "temporal_triple:SP",
+					Timestamp: 1506888000000000000,
+				},
+				{
+					Row:       "test:POS:91b6c9d2-69e3-5cec-a465-d200f4872671:e5364b94-fe66-5b01-a526-5fdf1e028e80:2821fefc-a86e-597b-b187-58805cad8813",
+					Column:    "temporal_triple:OS",
+					Timestamp: 1506888000000000000,
+				},
+				{
+					Row:       "test:SOP:2821fefc-a86e-597b-b187-58805cad8813:e5364b94-fe66-5b01-a526-5fdf1e028e80:91b6c9d2-69e3-5cec-a465-d200f4872671",
+					Column:    "temporal_triple:OP",
+					Timestamp: 1506888000000000000,
+				},
+				{
+					Row:       "test:SPO:2821fefc-a86e-597b-b187-58805cad8813:91b6c9d2-69e3-5cec-a465-d200f4872671:e5364b94-fe66-5b01-a526-5fdf1e028e80",
+					Column:    "temporal_triple:PO",
+					Timestamp: 1506888000000000000,
+				},
+			},
+		},
+		{
+			tpl: mustParse(t, `/u<joe> "parent_of"@[2017-12-21T12:00:00-08:00] /u<mary>`),
+			want: []*Indexer{
+				{
+					Row:       "test:OSP:e5364b94-fe66-5b01-a526-5fdf1e028e80:2821fefc-a86e-597b-b187-58805cad8813:91b6c9d2-69e3-5cec-a465-d200f4872671",
+					Column:    "temporal_triple:SP",
+					Timestamp: 1513886400000000000,
+				},
+				{
+					Row:       "test:POS:91b6c9d2-69e3-5cec-a465-d200f4872671:e5364b94-fe66-5b01-a526-5fdf1e028e80:2821fefc-a86e-597b-b187-58805cad8813",
+					Column:    "temporal_triple:OS",
+					Timestamp: 1513886400000000000,
+				},
+				{
+					Row:       "test:SOP:2821fefc-a86e-597b-b187-58805cad8813:e5364b94-fe66-5b01-a526-5fdf1e028e80:91b6c9d2-69e3-5cec-a465-d200f4872671",
+					Column:    "temporal_triple:OP",
+					Timestamp: 1513886400000000000,
+				},
+				{
+					Row:       "test:SPO:2821fefc-a86e-597b-b187-58805cad8813:91b6c9d2-69e3-5cec-a465-d200f4872671:e5364b94-fe66-5b01-a526-5fdf1e028e80",
+					Column:    "temporal_triple:PO",
+					Timestamp: 1513886400000000000,
+				},
+			},
+		},
+	}
+
+	for i, entry := range table {
+		if got, want := ForTriple(graph, entry.tpl), entry.want; !reflect.DeepEqual(got, want) {
+			msg := []string{
+				fmt.Sprintf("ForTriple:\nCase %d failed;\n", i),
+			}
+			for j, max := 0, len(got); j < max; j++ {
+				msg = append(msg, fmt.Sprintf("Row:\n\tgot :%v\n\twant:%v\n", got[j].Row, want[j].Row))
+				msg = append(msg, fmt.Sprintf("Col:\n\tgot :%v\n\twant:%v\n", got[j].Column, want[j].Column))
+				msg = append(msg, fmt.Sprintf("Cts:\n\tgot :%v\n\twant:%v\n", got[j].Timestamp, want[j].Timestamp))
+			}
+			t.Error(strings.Join(msg, ""))
+		}
+	}
+}
